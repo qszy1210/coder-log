ba66c76fd289f53ba01aa7fbf6f223de2a7a93e8
北山秋叶 <asdfqs@163.com>
2020-07-07 21:04:02 +0800

    2020-07-07 add cname file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-hexo deploy --generate;
-rm -rf qingsong
-rm -rf tech
-mv public tech
+# hexo deploy --generate;
+# rm -rf qingsong
+# rm -rf tech
+# mv public tech
+
+#!/bin/sh
+
+hexo clean
+hexo g
+hexo d
